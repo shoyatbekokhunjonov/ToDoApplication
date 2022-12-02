00a7ce66f508d70aec3c9c06428a1669873fa70c
Shoyatbek <shoyatbekpm@gmail.com>
2022-12-02 10:15:51 +0500

    Unit test changed, 1.

--- ToDoListApplicationTests/ToDoEntryRepo.Tests.cs
+++ ToDoListApplicationTests/ToDoEntryRepo.Tests.cs
@@ -17,24 +17,5 @@ namespace ToDoListApplicationTests
                 .Options;
         }
 
-        [Fact]
-        public Task ToDoEntryCreate()
-        {
-            // Arrange.
-            var toDoEntryContext = new ToDoListDbContext(dbContextOptions);
-            ToDoEntryRepo toDoEntryRepo = new ToDoEntryRepo(dbContextOptions);
-            var newToDoEntry = new ToDoEntry()
-            {
-                Id = 1,
-                Title = "Swimming",
-                Description = "Going to the pool",
-                DueDate = System.DateTime.Today.AddDays(1),
-                ToDoEntryStatus = okhunjonov_shoyatbek_todolist.Enums.ToDoEntryStatus.NotStarted,
-                ShowStatus = okhunjonov_shoyatbek_todolist.Enums.ToDoEntryShowHidden.Show
-            };
-
-            // Act.
-            ToDoEntryRepo.Create
-        }
     }
 }
