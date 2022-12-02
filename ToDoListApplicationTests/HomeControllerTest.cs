a9fe186228935232ce9cef3e008a1207f64fdbbf
Shoyatbek <shoyatbekpm@gmail.com>
2022-12-02 10:14:30 +0500

    Commit #1

---
+++ ToDoListApplicationTests/HomeControllerTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using okhunjonov_shoyatbek_todolist;
+using Microsoft.AspNetCore.Mvc.Testing;
+using okhunjonov_shoyatbek_todolist.Models.ViewModels;
+using okhunjonov_shoyatbek_todolist.IRepositories;
+
+namespace ToDoListApplicationTests
+{
+    public class HomeControllerTest : IClassFixture<WebApplicationFactory<okhunjonov_shoyatbek_todolist.Startup>>
+    {
+        
+
+        
+    }
+}
---
+++ ToDoListApplicationTests/ToDoEntryRepo.Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using okhunjonov_shoyatbek_todolist;
+using okhunjonov_shoyatbek_todolist.Models;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ToDoListApplicationTests
+{
+    public class ToDoEntryRepo
+    {
+        private readonly DbContextOptions<ToDoListDbContext> dbContextOptions;
+
+        public ToDoEntryRepo()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<ToDoListDbContext>()
+                .UseInMemoryDatabase("Test_Database")
+                .Options;
+        }
+
+        [Fact]
+        public Task ToDoEntryCreate()
+        {
+            // Arrange.
+            var toDoEntryContext = new ToDoListDbContext(dbContextOptions);
+            ToDoEntryRepo toDoEntryRepo = new ToDoEntryRepo(dbContextOptions);
+            var newToDoEntry = new ToDoEntry()
+            {
+                Id = 1,
+                Title = "Swimming",
+                Description = "Going to the pool",
+                DueDate = System.DateTime.Today.AddDays(1),
+                ToDoEntryStatus = okhunjonov_shoyatbek_todolist.Enums.ToDoEntryStatus.NotStarted,
+                ShowStatus = okhunjonov_shoyatbek_todolist.Enums.ToDoEntryShowHidden.Show
+            };
+
+            // Act.
+            ToDoEntryRepo.Create
+        }
+    }
+}
